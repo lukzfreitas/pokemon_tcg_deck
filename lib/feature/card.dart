62e711038e430fee556c26068c02a093285e4e1a
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-04-28 00:03:37 -0300

    Crreation card

--- lib/feature/card.dart
+++ lib/feature/card.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 
 class PokemonCard extends StatelessWidget {
   final String name;
+  final String level;
+  final String hp;
   final double height;
   final double width;
-  final String imageUrl;
+  final String imageCardUrl;
+  final String imageLogoUrl;
 
   const PokemonCard({
     Key? key,
     required this.name,
+    required this.level,
+    required this.hp,
     required this.height,
     required this.width,
-    required this.imageUrl,
+    required this.imageCardUrl,
+    required this.imageLogoUrl,
   }) : super(key: key);
 
   @override
@@ -35,27 +41,79 @@ class PokemonCard extends StatelessWidget {
               color: Colors.grey.withOpacity(0.2),
               spreadRadius: 4,
               blurRadius: 4,
-              offset: const Offset(0, 2), // changes position of shadow
+              offset: const Offset(0, 2),
             ),
           ]),
       child: Column(
-        children: [
-          header(context),
-          image(context)          
-        ],
+        children: [header(context), image(context), description(context)],
       ),
     );
   }
 
   Widget header(BuildContext context) {
-    return Text(name);
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 14,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: name,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                TextSpan(
+                  text: '  Level $level',
+                  style: const TextStyle(fontSize: 10),
+                ),
+              ],
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              text: 'HP $hp',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget image(BuildContext context) {
     return SizedBox(
-      height: 194,
-      width: width,
-      child: Image.network(imageUrl),
+      width: MediaQuery.of(context).size.width,
+      child: Image.network(imageCardUrl),
+    );
+  }
+
+  description(BuildContext context) {
+    return Container(
+      alignment: AlignmentDirectional.bottomStart,
+      margin: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Column(
+        children: [
+          RichText(
+            text: const TextSpan(
+              text: 'Fury Blaze',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          RichText(
+            text: const TextSpan(
+              text: 'If your opponent has 3 or less Prize cards left, each of Charizards attacks does 50 more damage to the Active Pokémon (before applying Weakness and Resistance).',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
